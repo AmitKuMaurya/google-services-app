c344126809831a12cb8c7061a959956f9e3898c7
[name] <jaipal@createbytes.com>
2023-12-02 12:57:07 +0530

    Application server is created using node and ts

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Print ", 10);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const PORT = 4444;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.get('/hey', (req, res, next) => {
+    res.status(200).send("Hey from me!!");
+});
+app.listen(PORT, () => console.table(`Listening on ${PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Print ",10);
+import express,{Request,Response,NextFunction, Application} from "express";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+const PORT = 4444;
+const app : Application = express();
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/hey',(req : Request,res:Response,next : NextFunction)=>{
+    res.status(200).send("Hey from me!!");
+})
+
+app.listen(PORT,()=> console.table(`Listening on ${PORT}`));
